083c517870d2c5e81bd7017338176e3f129ccb42
Kirill <121458409+Oknehcvotil@users.noreply.github.com>
2024-02-21 20:42:25 +0200

    Update project.tsx

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -33,7 +33,7 @@ export default function Project({
       }}
       className="mb-3 sm:mb-8 last:mb-0 group"
     >
-      <div className="relative max-w-[42rem] overflow-hidden rounded-lg border border-black/5 bg-gray-100 transition group-hover:bg-gray-200 dark:bg-white/10 dark:text-white dark:hover:bg-white/20 sm:h-[20rem]">
+      <div className="relative max-w-[42rem] overflow-hidden rounded-lg border border-black/5 bg-gray-100 transition group-hover:bg-gray-200">
         <div className="relative flex h-full flex-col px-5 pb-6 pt-4 sm:max-w-[50%] sm:pl-7 sm:pr-2 sm:pt-6 sm:group-even:ml-[19.5rem]">
           <h3 className="text-2xl hidden font-semibold text-center sm:block">
             {title}
