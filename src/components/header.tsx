6eeed21fc2ce13ecf52b6f8108d0b11947b2e94a
Kirill <121458409+Oknehcvotil@users.noreply.github.com>
2024-02-23 15:55:09 +0200

    Update header.tsx
    update color of active text in dark

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -31,9 +31,9 @@ export default function Header() {
               <Link
                 href={link.hash}
                 className={clsx(
-                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                  "group flex w-full items-center justify-center px-3 py-3 transition hover:text-gray-950  dark:hover:text-gray-200",
                   {
-                    "text-gray-950 dark:text-gray-300":
+                    "text-gray-950 dark:text-gray-200":
                       activeSection === link.name,
                   }
                 )}
